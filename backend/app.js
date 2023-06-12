0c630c289812cc73921d4bf8b4b23c857192ecd6
Reynmor <reynmor@ya.ru>
2023-06-12 18:39:05 +0300

    исправить cors

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,14 @@ const app = express();
 // Safety
 app.use(hemlet());
 app.use(limiter);
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3001',
+    'https://reynmor.nomoredomains.rocks',
+  ],
+  credentials: true,
+  maxAge: 30,
+}));
 
 // Connect to data base
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
